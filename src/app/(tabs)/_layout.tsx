4e171806ede9d3d2b808cdd93706d9aa746f58b2
Zidotech <zulu.devs@gmail.com>
2024-11-03 14:07:50 +0100

    restructure the codes and delete unused file

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,31 +1,39 @@
 import { Tabs } from "expo-router";
-import AntDesign from '@expo/vector-icons/AntDesign';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 export default function TabsLayout() {
-    return (
-        <Tabs screenOptions={{ tabBarActiveTintColor: 'black', tabBarShowLabel: true, }}>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    headerTitle: 'Home',
-                    
-                    tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />
-                }}
-            />
-            <Tabs.Screen
-                name="rota"
-                options={{
-                    headerTitle: 'Rota',
-                    tabBarIcon: ({ color }) => <MaterialIcons name="calendar-today" size={24} color={color} />
-                }}
-            />
-            <Tabs.Screen
-                name="settings"
-                options={{
-                    headerTitle: 'Settings',
-                    tabBarIcon: ({ color }) => <MaterialIcons name="settings" size={24} color={color} />
-                }}
-            />
-        </Tabs>
-    )
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: true }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rota"
+        options={{
+          headerTitle: "Rota",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="calendar-today" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          headerTitle: "Settings",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="settings" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
